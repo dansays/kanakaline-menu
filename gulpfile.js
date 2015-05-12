67cf67abb8a8c1b17f867f3481358e89884cf85b
Dan Budiac <dansays@gmail.com>
2015-05-11 21:30:06 -0400

    Add new gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,9 @@ var gulp 				= require('gulp')
 	, browserify	= require('gulp-browserify')
 ;
 
-var sources = {
-	sass: [
-		'builds/development/menu/sass/styles.scss'
-	]
-};
-
 gulp.task('watch', function() {
 	gulp.watch('builds/development/**/*.scss', ['sass']);
+	gulp.watch('builds/development/**/scripts/*.js', ['js']);
 });
 
 gulp.task('sass', function() {
@@ -23,8 +18,26 @@ gulp.task('sass', function() {
 			style: 'expanded'
 		}).on('error', gutil.log))
 		.pipe(gulp.dest('builds/development/menu'));
+		
+	gulp.src('/builds/development/controller/sass/styles.scss')
+		.pipe(compass({
+			sass: 'builds/development/controller/sass',
+			image: 'builds/development/controller/assets/images',
+			style: 'expanded'
+		}).on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/controller'));	
 });
 
 gulp.task('js', function() {
+	gulp.src('builds/development/menu/scripts/*.js')
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/menu'));
 
+	gulp.src('builds/development/controller/scripts/*.js')
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/controller'));
 });
+
+gulp.task('default', ['sass', 'js', 'watch']);
